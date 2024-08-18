8319cada16cd8993e2d59aed0b57ba0ecd46c65d
yussuf1020 <39417103+yussuf1020@users.noreply.github.com>
2024-08-18 21:24:25 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/shoppinglistrev/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistrev/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background) {
-                    ShoppingListTest()
+                    ShoppingList()
                 }
             }
         }
